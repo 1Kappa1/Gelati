c032fae9b106c94096b1431ad42a85d241426c99
Capanna Alessandro <116791222+1Kappa1@users.noreply.github.com>
2023-05-18 09:19:30 +0200

    Add files via upload

---
+++ capanna.alessandro.4H.Gelati/capanna.alessandro.4H.gelati/Models/Gelato.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capanna.alessandro._4H.gelati.Models
+{
+    public class Gelato
+    {
+        public string Tipo { get; set; }
+        public string Size { get; set; }
+
+        public string Gusto { get; set; }
+
+        public Gelato(string str) 
+        {
+            string [] colonne = str.Split(";");
+            this.Tipo= colonne[0];
+            this.Size = colonne[1];
+            this.Gusto= colonne[2];
+        }
+    }
+
+    public class Gelati:List<Gelato> 
+    { 
+        public Gelati() 
+        { 
+        }
+
+        public Gelati(string File_Name)
+        {
+            using(StreamReader fin= new StreamReader(File_Name))
+            { 
+                while(!fin.EndOfStream)
+                {
+                    string str=fin.ReadLine();
+                    Gelato g=new Gelato(str);
+                    Add(g);
+                }    
+            }
+        }
+    }
+}
